8c0108a0396799f1e109721a94a4bf0b3c234310
Nabila Ayu Talita <nabilaayutalitaa@gmail.com>
2025-12-01 01:24:39 +0700

    feat: add component and constants folder

--- ayu/jobskuy/app/index.tsx
+++ ayu/jobskuy/app/index.tsx
@@ -1,16 +1,23 @@
-import { Text, View } from "react-native";
-import { Stack } from "expo-router";
+import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { Stack, useRouter } from "expo-router";
+import { useState } from "react";
+
+import { COLORS, icons, SIZES } from "./constants";
+import { NearbyJobs, PopularJobs, ScreenHeaderBtn, Welcome } from "./components";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <SafeAreaView
+      className="flex-1"
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
     >
       <Text className="text-5xl font-bold">Jobskuy</Text>
-    </View>
+    </SafeAreaView>
   );
 }
